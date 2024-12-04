93eaddb134c80f2bb2412cde6f0e0806178b2cd2
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-12-04 03:09:28 +0100

    Add files via upload

---
+++ ll.py
@@ -0,0 +1,6 @@
+# Boucle pour parcourir les nombres de 8000 à 8999
+for nombre in range(8000, 9000):
+    # Vérifier si le nombre est pair en utilisant l'opérateur modulo (%)
+    if nombre % 2 == 0:
+        # Si le nombre est pair, l'afficher
+        print(nombre)
